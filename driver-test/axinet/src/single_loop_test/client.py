c353890291462dbc8ffa5797aa7dd3d2823170a4
zflcs <1491657576@qq.com>
2024-03-29 23:05:15 +0800

    Update single_loop_poll results

--- driver-test/axinet/src/single_loop_test/client.py
+++ driver-test/axinet/src/single_loop_test/client.py
@@ -14,41 +14,39 @@ MODE = sys.argv[1]
 MTU = int(sys.argv[2])
 
 
-TIME_OUT = 3
+TIME_OUT = 10
 raw_socket = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.htons(0x1234))
 raw_socket.bind(("enp4s0", 0))
 packet = struct.pack("!6s6sH", b"\x00\x0a\x35\x01\x02\x03", b"\x04\x7c\x16\xef\x34\xd1", 0x1234)
-count = 0
 delays = []
 
-for _ in range(10):
-    start_time = time.time()
-    while True:
-        if time.time() -start_time > TIME_OUT:
-            break
-        raw_socket.send(packet + b'a' * (MTU - 14))
-        start = time.time()
-        data = raw_socket.recv(MTU)
-        end = time.time()
-        delays.append((end - start) * 1000000)
-        count += 1
-        time.sleep(0.000000001)
-    # print("throughput:" + str(count / TIME_OUT))
-    # print("avarage delay:" + str(sum(delays) / count) + "us")
+start_time = time.time()
+while True:
+    if time.time() -start_time > TIME_OUT:
+        break
+    raw_socket.send(packet + b'a' * (MTU - 14))
+    start = time.time()
+    data = raw_socket.recv(MTU)
+    end = time.time()
+    delays.append((end - start) * 1000000)
+    time.sleep(0.000000001)
+
+
+pps = len(delays) / TIME_OUT
 avarage = np.mean(delays)
 std_deviation = np.std(delays)
 
 path = "../../assets/single_loop/" + MODE
 if os.path.exists(path + "/" + MODE + ".dat"):
     with open(path + "/" + MODE + ".dat", mode='a', encoding='utf-8') as file:
-        content = "{:-5d}\t\t\t{:07.3f}\t\t\t\t{:07.3f}".format(MTU, avarage, std_deviation)
+        content = "{:-5d}\t\t\t{:07.3f}\t\t\t\t{:07.3f}\t\t\t\t{:07.1f}".format(MTU, avarage, std_deviation, pps)
         file.write(content + "\n")
 else:
     os.mkdir(path)
     os.chmod(path, stat.S_IRWXO + stat.S_IRWXG + stat.S_IRWXU)
     with open(path + "/" + MODE + ".dat", mode='w', encoding='utf-8') as file:
-        file.write("Frame_size(Bytes)\tAvarage(us)\t\t\tStd_deviation\n")
-        content = "{:-5d}\t\t\t{:07.3f}\t\t\t\t{:07.3f}".format(MTU, avarage, std_deviation)
+        file.write("Frame_size(Bytes)\tAvarage(us)\t\t\tStd_deviation\t\t\tThroughput(pps)\n")
+        content = "{:-5d}\t\t\t{:07.3f}\t\t\t\t{:07.3f}\t\t\t\t{:07.1f}".format(MTU, avarage, std_deviation, pps)
         file.write(content + "\n")
     os.chmod(path + "/" + MODE + ".dat", stat.S_IRWXO + stat.S_IRWXG + stat.S_IRWXU)
 
